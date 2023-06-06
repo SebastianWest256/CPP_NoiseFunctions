c62a10c0c70770139900e54e13f0a107056605fc
Sebastian West <123683338+SebastianWest256@users.noreply.github.com>
2023-06-06 16:25:46 -0500

    Added logarithm  and map functions

--- MathFunctions.cpp
+++ MathFunctions.cpp
@@ -1,3 +1,5 @@
+//useful mathematical functions
+
 float randomFloat(float min, float max) {
     float random = ((float)rand()) / (float)RAND_MAX;
     float range = max - min;
@@ -12,4 +14,19 @@ float lerp(float start, float end, float t) {
     return start + t * (end - start);
 }
 
+float map(float input, float inputMin, float inputMax, float targetMin, float targetMax) {
+    // Calculate the input value's position within the input range
+    float inputRange = inputMax - inputMin;
+    float normalizedInput = (input - inputMin) / inputRange;
+
+    // Map the normalized input to the target range
+    float targetRange = targetMax - targetMin;
+    float mappedValue = targetMin + (normalizedInput * targetRange);
+
+    return mappedValue;
+}
+
+double logarithm(double base, double x) {
+    return log(x) / log(base);
+}
 
